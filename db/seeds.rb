16f5ceb5fba5bb4ae9672c17d0282238d70c4c68
Katalina Nadasi <katalina.nadasi@gmail.com>
2015-04-28 17:45:31 +0200

    add new controllers Shoes and Brand errors fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@
 #
 Brand = Brand.create(name: "Adidas")
 Shoe = Shoe.create(name:"gazelle", brand: brand, description: "clacladsdsdsdsdsdsd")
+Shoe.create(name: 'Nike Air Max 95', brand: brand, description:"La chaussure Nike Air Max 95 pour
+ Homme possède le même style emblématique et le même amorti exceptionnel que le modèle dont elle
+ est inspirée. Sortie en 1995, l'originale a été plébiscitée par les coureurs."),
 
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Adidas_list = ['Adidas Stan Smith', 'Adidas Superstar', 'Adidas ZX Flux',
+# 'Adidas Energy Boost', 'Adidas Originals Tubular', 'Adidas Yeezy 750',
+# 'Adidas Superstar', 'Adidas Gazelle', 'Adidas nastase']
 
-# modele de chaussures
 # size (36..46)
