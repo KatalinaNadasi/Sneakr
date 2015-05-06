a37b839f15345b9200489a0588ee64b33c62794f
Katalina Nadasi <katalina.nadasi@gmail.com>
2015-05-06 15:00:19 +0200

    add formulaire de recherche

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,5 +2,15 @@ class ResultsController < ApplicationController
   def index
     @shoe = Shoe.find(params[:shoe_id])
     @results = @shoe.results.order("price asc")
+
+    price = params[:amount]
+    size = params[:size]
+    if price && price.present?
+      @results = @results.where("price <= ?", price.to_f)
+    end
+
+    if size
+      @results = @results.where("size = ?", size)
+    end
   end
 end
