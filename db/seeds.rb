abf478899f1a8d5d473f20b447daa9f3c208c68b
Katalina Nadasi <katalina.nadasi@gmail.com>
2015-05-08 10:56:58 +0200

    enlever le if

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,8 @@ data = YAML.load_file(File.join(Rails.root, "db", "seed.yml"))
 
 data.each do |brand_data|
   brand = Brand.create(name: brand_data["name"], photo_url: brand_data["photo_url"])
-  if brand_data
-    brand_data["shoes"].each do |shoe_data|
-      Shoe.create(name: shoe_data["name"], description: shoe_data["description"], brand: brand, picture: open(shoe_data["url"]))
-    end
+  brand_data["shoes"].each do |shoe_data|
+    Shoe.create(name: shoe_data["name"], description: shoe_data["description"], brand: brand, picture: open(shoe_data["url"]))
   end
 end
 
